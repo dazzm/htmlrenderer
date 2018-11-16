336d764839ab30023c789e9436e095bb406b2471
dazzm <mazam@pipingrock.com>
2018-11-16 18:46:52 +0530

    added render route

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const {json, send} = require('micro')
-const {router, get, post} = require('microrouter')
+const {router, get} = require('microrouter')
 const axios = require('axios')
 
 
 module.exports = router(
-  get('/', async (req, res) => `hello there, whatsup`),
 
+  get('/', (req, res) => 'you can render'),
+
+  get('/render/:url', async (req, res) => {
+    const response = await axios(req.params.url)
+    send(res, 200, response.data)
+
+  })
+  
 )
